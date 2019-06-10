0cb05cac417dbf4ba0691a791ecb7c87c38520c1
Lili Boxer <ninaliboxer@gmail.com>
2019-06-10 11:46:41 -0700

    lot list dynamically populating

--- src/home-page/LotItem.js
+++ src/home-page/LotItem.js
@@ -2,9 +2,10 @@ import Component from '../Component.js';
 
 class LotItem extends Component {
     renderTemplate() {
+        const lot = this.props.lot;
         return /*html*/`
         <li>
-            <a>Electronics</a>
+            <a>${lot.lotName}</a>
         </li>
         `;
 
--- src/home-page/LotList.js
+++ src/home-page/LotList.js
@@ -1,9 +1,22 @@
 import Component from '../Component.js';
+import LotItem from './LotItem.js';
 
 class LotList extends Component {
+    render() {
+        const list = this.renderDOM();
+        const lots = this.props.lots;
+
+        lots.forEach(lot => {
+            const lotItem = new LotItem({ lot });
+            list.appendChild(lotItem.render());
+        });
+
+        return list;
+    }
+
     renderTemplate() {
         return /*html*/`
-            <ul></ul>
+            <ul>LIST</ul>
         `;
 
     }
