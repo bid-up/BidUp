b0cd3cc4d24075a5ac8228431229658c5cb91137
Lili Boxer <ninaliboxer@gmail.com>
2019-06-10 09:36:35 -0700

    configs dropped in

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,11 @@
 const config = {
-    // config goes here
+    apiKey: 'AIzaSyAKCVpeBXoCP9K1F4azCmIdBC6CzUdsZ18',
+    authDomain: 'bidup-e0118.firebaseapp.com',
+    databaseURL: 'https://bidup-e0118.firebaseio.com',
+    projectId: 'bidup-e0118',
+    storageBucket: 'bidup-e0118.appspot.com',
+    messagingSenderId: '887377005660',
+    appId: '1:887377005660:web:accab8653d26664c'
 };
 
 export const app = firebase.initializeApp(config);
