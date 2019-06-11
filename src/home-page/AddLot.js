3e3291561122c497e3c1800d072df5ed64adf422
Wes Griffin <themisguidedidiot@gmail.com>
2019-06-11 15:13:30 -0700

    seperated lot name and products into two different forms. Loads to firebase

--- src/home-page/AddLot.js
+++ src/home-page/AddLot.js
@@ -6,7 +6,6 @@ class AddLot extends Component {
         const dom = this.renderDOM();
         const form = dom.querySelector('form');
         const lotNameInput = form.querySelector('input[name=lot-name]');
-        console.log(form);
 
         const addLotFormButton = dom.querySelector('.add-lot-form-button');
 
@@ -15,42 +14,50 @@ class AddLot extends Component {
             modal.style.display = 'block';
         });
 
+        let currentLotRef = null;
+
+        //event listener for Lot
         form.addEventListener('submit', event => {
             event.preventDefault();
             const formData = new FormData(form);
 
             const lotRef = lotsRef.push();
+            currentLotRef = lotRef;
 
             lotRef.set({ 
                 key: lotRef.key,
                 lotName: formData.get('lot-name'),
                 owner: auth.currentUser.uid
             });
+        });
 
+        const productForm = dom.querySelector('.modal-products');
+        
+        productForm.addEventListener('submit', event => {
+            event.preventDefault();
+            const formData = new FormData(productForm);
             const productRef = productsRef.push();
-            console.log(productRef, 'prodref');
             
             productRef.set({ 
-                lotKey: lotRef.key, //might not need
+                lotKey: currentLotRef.key, //might not need
                 key: productRef.key,
                 productName: formData.get('product-name'),
                 productURL: formData.get('product-image')
             });
-            console.log(formData.get('product-name'));
 
             productsByLotRef
-                .child(lotRef.key)
+                .child(currentLotRef.key)
                 .child(productRef.key)
                 .set({
                     key: productRef.key
                 });
 
-            // form.reset();
+            form.reset();
             lotNameInput.focus();
             document.activeElement.blur();
-
+        
         });
-
+      
         const modal = form.querySelector('#myModal');
 
         window.onclick = function(event) {
@@ -76,13 +83,14 @@ class AddLot extends Component {
             <div id="myModal" class="modal">
                 <form class="modal-content">
                     <label>Lot Name: <input name="lot-name"></label>
-                    <label>Product Name: <input name="product-name"></label>
-                    <label>Product Image URL: <input name="product-image"></label>
-                    <label>Product Name: <input name="product-name"></label>
-                    <label>Product Image URL: <input name="product-image"></label>
-                    <button class="add-lot-to-database">Add</button>
+                    <button class="add-lot-to-database">Add Lot</button>
                 </form>
-                <button id="close-modal" class="close-modal">Close</button>
+                <form class="modal-products">
+                    <label>Product Name: <input name="product-name"></label>
+                    <label>Product Image URL: <input name="product-image"></label>
+                    <button class="add-products-to-database">Add Products</button>
+                </form>
+                <button id="close-modal" class="close-modal">DONE</button>
             </div>
 
             <button class="add-lot-form-button">Add Room and Items</button>
