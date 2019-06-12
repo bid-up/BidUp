569d89cd6a69c560e6c7465d2883bdbd23227f03
Lili Boxer <ninaliboxer@gmail.com>
2019-06-12 13:25:38 -0700

    bidding 10 button delay working!

--- src/bidding/MakeBid.js
+++ src/bidding/MakeBid.js
@@ -10,53 +10,76 @@ class MakeBid extends Component {
 
         const lot = this.props.lot;
         
-        bidTen.addEventListener('click', () => {
-            const bidAmount = 10;
+        function successfulBid(bidAmount, val) {
+            // update reset timer in db
+            activeLotsRef
+                .child(lot.key)
+                .child('resetTimer')
+                .set({
+                    reset: new Date().getTime() // change to UID
+                });
 
+            // change holding balance 
+            const updatedHoldingBalance = val.holdingBalance - bidAmount;
+            usersByLotRef
+                .child(lot.key)
+                .child(auth.currentUser.uid)
+                .child('holdingBalance')
+                .set({
+                    holdingBalance: updatedHoldingBalance
+                });
+
+            // get highest bid
+            let highestBid;
+            usersByLotRef
+                .child(lot.key)
+                .child(auth.currentUser.uid)
+                .child('highestBid')
+                .once('value', snapshot => {
+                    const value = snapshot.val();
+                    value 
+                        ? highestBid = value.highestBid 
+                        : highestBid = 0;
+                                
+                    // set highest bid
+                    usersByLotRef
+                        .child(lot.key)
+                        .child(auth.currentUser.uid)
+                        .child('highestBid')
+                        .set({
+                            highestBid: highestBid + bidAmount
+                        });
+                });
+        }
+
+        activeLotsRef
+            .child(lot.key)
+            .child('timeRemaining')
+            .on('value', snapshot => {
+                const val = snapshot.val();
+                if(val.time >= 5) {
+                    bidTen.disabled = true;
+                    bidFifty.disabled = true;
+                } else {
+                    bidTen.disabled = false;
+                    bidFifty.disabled = false;
+                }
+            });
+
+        bidTen.addEventListener('click', () => {
+            console.log('hello')
+            const bidAmount = 10;
+            // get holding balance
             usersByLotRef
                 .child(lot.key)
                 .child(auth.currentUser.uid)
                 .child('holdingBalance')
                 .once('value', snapshot => {
                     const val = snapshot.val();
+                    // check if user has enough money to bid
                     if(val.holdingBalance >= bidAmount) {
-                        // update reset timer in db
-                        activeLotsRef
-                            .child(lot.key)
-                            .child('resetTimer')
-                            .set({
-                                reset: new Date().getTime() // change to UID
-                            });
-                        // change holding balance 
-                        const updatedHoldingBalance = val.holdingBalance - bidAmount;
-                        usersByLotRef
-                            .child(lot.key)
-                            .child(auth.currentUser.uid)
-                            .child('holdingBalance')
-                            .set({
-                                holdingBalance: updatedHoldingBalance
-                            });
-                        // get highest bid
-                        let highestBid;
-                        usersByLotRef
-                            .child(lot.key)
-                            .child(auth.currentUser.uid)
-                            .child('highestBid')
-                            .once('value', snapshot => {
-                                const value = snapshot.val();
-                                value 
-                                    ? highestBid = value.highestBid 
-                                    : highestBid = 0
-                                            
-                                // set highest bid
-                                usersByLotRef
-                                    .child(lot.key)
-                                    .child(auth.currentUser.uid)
-                                    .child('highestBid')
-                                    .set({
-                                        highestBid: highestBid + bidAmount
-                                    });
-                            });
+                        successfulBid(bidAmount, val);
+                        console.log('bid success');
                     }    
                 });
     
@@ -65,16 +88,6 @@ class MakeBid extends Component {
     }
     
     renderTemplate() {
-        const isDisabled = this.props.isDisabled;
-
-        if(isDisabled) {
-            return /*html*/ `
-            <div>
-                <button class="bid-ten" disabled>10</button>
-                <button class="bid-fifty" disabled>50</button>
-            </div>
-            `;
-        }
 
         return /*html*/ `
             <div>
