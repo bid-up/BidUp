90debdd8bdac861f83cff4ca42899aa1a2b9677e
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-13 09:40:06 -0700

    added images for about page

--- src/about/AboutApp.js
+++ src/about/AboutApp.js
@@ -18,7 +18,38 @@ class AboutApp extends Component {
             <div>
                 <main>
                     <h1>About The Team</h1>
-                    
+                        <div>
+                            <h2>Carlos</h2>
+                            <img src="assets/about/carlos.png">
+                            <ul>
+                                <li><a href=""><img src=></a></li>
+                                <li><a href=""><img src=></a><li>
+                                <li><a href=""><img src=""></a></li>
+                            </ul>
+                        </div>
+
+                    <div>
+                    <h2>Leigh-Ann</h2>
+                    <img src="fdaf">
+                    <span>
+                    <a href=""></a>
+                    </span>
+                    </div>
+
+                    <div>
+                    <h2>Lili</h2>
+                    <img src="fdaf">
+                    </div>
+
+                    <div>
+                    <h2>Susan</h2>
+                    <img src="fdaf">
+                    </div>
+
+                    <div>
+                    <h2>Wes</h2>
+                    <img src="fdaf">
+                    </div>
                 </main>
             </div>
         `;
