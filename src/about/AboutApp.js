4d23750360349f152b4e6b38f5a51fb54a718b19
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-13 10:09:55 -0700

    static data, about us

--- src/about/AboutApp.js
+++ src/about/AboutApp.js
@@ -22,34 +22,47 @@ class AboutApp extends Component {
                             <h2>Carlos</h2>
                             <img src="assets/about/carlos.png">
                             <ul>
-                                <li><a href=""><img src=></a></li>
-                                <li><a href=""><img src=></a><li>
-                                <li><a href=""><img src=""></a></li>
+                                <li><a href="https://github.com/carlosus"><img src="assets/about/github.png"></a></li>
+                                <li><a href="https://www.linkedin.com/in/carlosussantiago/"><img src="assets/about/linkedin.png"></a></li>
+                                <li><img src="assets/about/twitter.png"></li>
                             </ul>
                         </div>
-
-                    <div>
-                    <h2>Leigh-Ann</h2>
-                    <img src="fdaf">
-                    <span>
-                    <a href=""></a>
-                    </span>
-                    </div>
-
-                    <div>
-                    <h2>Lili</h2>
-                    <img src="fdaf">
-                    </div>
-
-                    <div>
-                    <h2>Susan</h2>
-                    <img src="fdaf">
-                    </div>
-
-                    <div>
-                    <h2>Wes</h2>
-                    <img src="fdaf">
-                    </div>
+                        <div>
+                        <h2>Leigh-Ann</h2>
+                        <img src="assets/about/leigh-ann.jpeg">
+                            <ul>
+                                <li><a href="https://github.com/lacrivella"><img src="assets/about/github.png"></a></li>
+                                <li><a href="https://www.linkedin.com/in/lacrivella/"><img src="assets/about/linkedin.png"></a></li>
+                                <li><a href="https://twitter.com/lacrivella"><img src="assets/about/twitter.png"></a></li>
+                            </ul>
+                        </div>
+                        <div>
+                        <h2>Lili</h2>
+                        <img src="assets/about/lili.jpeg">
+                            <ul>
+                                <li><a href="https://github.com/liliboxer"><img src="assets/about/github.png"></a></li>
+                                <li><a href="https://www.linkedin.com/in/lili-boxer/"><img src="assets/about/linkedin.png"></a></li>
+                                <li><a href="https://twitter.com/liliboxer"><img src="assets/about/twitter.png"></a></li>
+                            </ul>
+                        </div>
+                        <div>
+                        <h2>Susan</h2>
+                        <img src="assets/about/susan.jpeg">
+                            <ul>
+                                <li><a href="https://github.com/sepuckett86"><img src="assets/about/github.png"></a></li>
+                                <li><a href="https://www.linkedin.com/in/susanpuckett/"><img src="assets/about/linkedin.png"></a></li>
+                                <li><a href="https://twitter.com/SusanPu29823252"><img src="assets/about/twitter.png"></a></li>
+                            </ul>
+                        </div>
+                        <div>
+                        <h2>Wes</h2>
+                        <img src="assets/about/wes.jpeg">
+                            <ul>
+                                <li><a href="https://github.com/codingclueless"><img src="assets/about/github.png"></a></li>
+                                <li><a href="https://www.linkedin.com/in/wes-griffin-319b7a184/"><img src="assets/about/linkedin.png"></a></li>
+                                <li><img src="assets/about/twitter.png"></li>
+                            </ul>
+                        </div>
                 </main>
             </div>
         `;
