dc4295615867a309d3365ff0a6efc26b441cba16
Lili Boxer <ninaliboxer@gmail.com>
2019-06-13 17:00:24 -0700

    fonts for header updated, rearranged bidder page

--- src/bidding/BidderBalance.js
+++ src/bidding/BidderBalance.js
@@ -3,11 +3,9 @@ import Component from '../Component.js';
 class BidderBalance extends Component {
     renderTemplate() {
         const balance = this.props.balance;
-        const holdingBalance = this.props.holdingBalance;
 
         return /*html*/ `
         <div>
-            <p>Available Betting Funds: ${holdingBalance}</p>
             <p>Your Total Balance: ${balance}</p>
         </div>
         `;
